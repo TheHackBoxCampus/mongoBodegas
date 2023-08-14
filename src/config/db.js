bd9ece08ccfed8fe0f90cae6bd01d8a80b168bca
M1LL$R N4R1Ñ0 <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-08-14 08:47:09 -0500

    feat(Root): Conexion base de datos
    Se crea la conexion a la base de datos de mongo

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+import globalProperties from "../env/env.js";
+
+const conx = () => {
+    try {
+        let credentials = JSON.parse(globalProperties.USERDB);
+        let uri = `mongodb+srv://${credentials.username}:${credentials.password}@cluster0.lk9oe5f.mongodb.net/${credentials.database}`;
+        let options = {
+            useNewUrlParser: true,
+            useunifiedtopology: true
+        }
+        let client = new MongoClient.connect(uri, options);
+        console.log("db --> success");
+        return client.db(); 
+    }catch(err) {
+        console.error(err.message)
+    }
+}
+
+export default conx; 
