93984e8cc015864bbf8c3d63c0eef5a4516f24dc
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-15 05:01:04 -0500

    feat(Root): Utilizacion de morgan
    Se utilizo morgan para estar al pendiente de las peticiones
    
    ejemplo de uso:
    Haces una peticion a generar/bodegas
    
    morgan en la consola te da una previsualizacion de esa consulta y su estado
    
    GET /generar/bodegas 200 1.504 ms - 434

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import morgan from "morgan";
+import auth from "./router/auth.router.js";
 
 const app = express(); 
 
@@ -8,10 +10,12 @@ const app = express();
 
 app.use(express.json())
 app.use(express.text())
-
+app.use(morgan("dev"))
 
 /**
  * Routes
  */
+
+app.use(auth);
 
 export default app; 
