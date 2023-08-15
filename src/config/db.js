a012394f094ce1fd597a719002bcbe5e2b04a65a
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-15 09:40:22 -0500

    fix(Root): Conexion con mongo
    Se arreglo la sintaxis de la  conexion con mongodb

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 import globalProperties from "../env/env.js";
 
-const conx = () => {
+const conx = async () => {
     try {
         let credentials = JSON.parse(globalProperties.USERDB);
         let uri = `mongodb+srv://${credentials.username}:${credentials.password}@cluster0.lk9oe5f.mongodb.net/${credentials.database}`;
@@ -9,7 +9,7 @@ const conx = () => {
             useNewUrlParser: true,
             useunifiedtopology: true
         }
-        let client = new MongoClient.connect(uri, options);
+        let client = await MongoClient.connect(uri, options);
         console.log("db --> success");
         return client.db(); 
     }catch(err) {
