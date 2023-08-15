e262baaa9692ba8ae72508ff3d6fc0cca14c0250
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-15 05:01:42 -0500

    feat(Root): Controlador de tokens especificos
    Se creo un controlador para crear un token dependiendo de la categoria que escoga el usuario

---
+++ src/controller/generate.category.js
@@ -0,0 +1,34 @@
+import "reflect-metadata";
+import { classToPlain, plainToClass } from "class-transformer";
+import globalProperties from "../env/env.js"
+import jwt from "jsonwebtoken"
+import Usuarios from "../storage/structure/usuarios.js"
+import Bodegas from "../storage/structure/bodegas.js"
+
+const getClass = (cls) => {
+    let classesAvailables = {
+        'usuarios': Usuarios, 'bodegas': Bodegas
+    }  
+
+    let isMatch = classesAvailables[cls];
+    if(!isMatch) throw {status: 500, message: "Categoria incorrecta"}
+    else return {plain: plainToClass(isMatch, {}, {ignoreDecorators:true})}
+}
+
+const generateTokenSpecific = async (req, res, next) => {
+    try { 
+        let structure = getClass(req.params.categoria).plain; 
+        let token = new Promise((resolve, reject) => {
+            jwt.sign(Object.assign({}, classToPlain(structure)), globalProperties.KEY, {algorithm: "HS256", expiresIn: "10m"}, (err, token) => {
+                 err ? reject(err) : resolve(token) 
+            })
+        })
+        let getToken = await token; 
+        req.categoria = {status: 200, token: getToken, categoria: req.params.categoria}
+        return next() 
+    }catch(err) {
+        res.status(500).send({status: 500, message: err.message})
+    }
+}
+
+export default generateTokenSpecific; 
