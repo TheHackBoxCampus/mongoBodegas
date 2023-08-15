6714315c9ffac0ff60183a4ed21c6df14ae05833
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-15 05:02:51 -0500

    feat(Root): Configuraciones de la dependencia rate-limit
    Se justifican las configuraciones para tener un control de las peticiones

---
+++ src/limits/settings.limits.js
@@ -0,0 +1,19 @@
+import { rateLimit } from "express-rate-limit";
+
+const requests = rateLimit({
+    windowMs: 30 * 1000,
+    max: 5,
+    legacyHeaders: true,
+    standardHeaders: true, 
+    skip: (req, res) => {
+         if(req.headers["content-length"] > 150) {
+             res.status(413).send({status: 413, message: "la cantidad de bytes es mayor a la esperada"})
+             return true
+        }
+    },
+    message: (req, res) => {
+        res.status(429).send({status: 429, message: "Limite de peticiones alcanzada"}) 
+    }   
+})
+
+export default requests; 
