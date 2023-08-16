5c5b7af1f43be3b2ec4c89b3140e4bceb58329f1
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-16 09:23:44 -0500

    feat(root): Nueva ruta para productos
    Se genero otra ruta para la consulta que me permite actualizar o insertar dependiendo de los patrones y productos ya existentes

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -4,6 +4,7 @@ import generateTokenSpecific from "../controller/generate.category.js";
 import { warehouseSort, warehousesPush } from "../controller/bodegas.sort.js";
 import countOfProducts from "../controller/bodegas.count.js";
 import { customAuthPassport } from "../middleware/verifyToken/jwtVerify.js";
+import { verifyPatterns } from "../controller/productos.news.js";
 import bodegasMD from "../middleware/bodegas.push.md.js";
 import Bodegas from "../storage/structure/bodegas.js";
 import Inventarios from "../storage/structure/inventarios.js";
@@ -16,6 +17,7 @@ auth.get("/generar/:categoria", requests, generateTokenSpecific, (req, res) => {
 
 auth.get("/bodegas/orden", requests, customAuthPassport(Bodegas), warehouseSort);
 auth.get("/productos/inventario/cantidad", requests, customAuthPassport(Inventarios), countOfProducts)
+auth.post("/productos/nueva", requests, customAuthPassport(Inventarios), verifyPatterns)
 auth.post("/bodegas/nueva", requests, customAuthPassport(Bodegas), bodegasMD, warehousesPush); 
 
 
