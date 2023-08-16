0ae5fcc24861bd7d96f6c51cfa1b46f0455e59f9
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-16 05:03:14 -0500

    feat(root): Validacion de datos entrantes
    Se valido la existencia de datos, primero me valida la existencia y despues el contenido

--- src/middleware/bodegas.push.md.js
+++ src/middleware/bodegas.push.md.js
@@ -5,10 +5,13 @@ import Bodegas from "../storage/structure/bodegas.js";
 
 const bodegasMD = async (req, res, next) => {
     try {
-        let data = plainToClass(Bodegas, req.body, {excludeExtraneousValues: true});
-        await validate(data);
-        req.data = JSON.parse(JSON.stringify(data)); 
-        return next()
+        if (Object.entries(req.body).length < 1) throw 'Parametros requeridos'
+        else {
+            await validate(Bodegas, req.body);
+            let data = plainToClass(Bodegas, req.body, {excludeExtraneousValues: true})
+            req.data = JSON.parse(JSON.stringify(data)); 
+            return next()
+        }
     }catch(err)  {  
         res.status(500).send({status: 500, message: err})
     }   
