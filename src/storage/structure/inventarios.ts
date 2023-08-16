8f32d19dc1fc7c9ac4471b9f03749a587b98ad8b
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-16 07:49:39 -0500

    feat(root): Estructura dto de inventarios
    Se agrega la estructura de datos del dto de inventarios para la validacion de endpoints y permisos

---
+++ src/storage/structure/inventarios.ts
@@ -0,0 +1,50 @@
+import { Expose } from "class-transformer"
+import  { IsDefined } from "class-validator"
+
+class Inventarios {
+     @Expose({name: "id_bodega"})
+     @IsDefined({ message: () => { throw "Parametro id_bodega requerido" }})
+     idb: number 
+    
+     @Expose({name: "id_producto"})
+     @IsDefined({ message: () => { throw "Parametro id_producto requerido" }})
+     idp: number 
+    
+     @Expose({name: "cantidad"})
+     @IsDefined({ message: () => { throw "Parametro cantidad requerido" }})
+     cnt: number 
+
+     @Expose({name: "created_by"})
+     @IsDefined({ message: () => { throw "Parametro created_by requerido" }})
+     crb: number 
+
+     @Expose({name: "update_by"})
+     @IsDefined({ message: () => { throw "Parametro update_by requerido" }})
+     urb: number 
+
+     @Expose({ name: "created_at" })
+     @IsDefined({ message: () => { throw "Parametro created_at requerido" } })
+     crat: Date
+   
+     @Expose({ name: "update_at" })
+     @IsDefined({ message: () => { throw "Parametro update_at requerido" } })
+     urat: Date
+   
+     @Expose({ name: "deleted_at" })
+     @IsDefined({ message: () => { throw "Parametro deleted_at requerido" } })
+     drat: Date
+
+     constructor(user:Partial<Inventarios>) {
+        Object.assign(this, user); 
+        this.idb = 0
+        this.idp = 0
+        this.cnt = 0
+        this.crb = 1   
+        this.urb = 1   
+        this.crat = new Date(2023, 8, 15)
+        this.urat = new Date(2023, 8, 15)
+        this.drat = new Date(2023, 8, 15)
+     }
+}
+
+export default Inventarios;
