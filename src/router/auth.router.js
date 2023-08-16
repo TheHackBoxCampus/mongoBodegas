efca516461cc4d60cb0e5f8336a778dace213af9
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-16 07:47:00 -0500

    feat(root): Nueva ruta
    Se agrega la ruta para realizar la consulta de inventarios y la cantidad de productos por bodega

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import requests from "../limits/settings.limits.js";
 import generateTokenSpecific from "../controller/generate.category.js";
 import { warehouseSort, warehousesPush } from "../controller/bodegas.sort.js";
+import countOfProducts from "../controller/bodegas.count.js";
 import { customAuthPassport } from "../middleware/verifyToken/jwtVerify.js";
 import bodegasMD from "../middleware/bodegas.push.md.js";
 import Bodegas from "../storage/structure/bodegas.js";
+import Inventarios from "../storage/structure/inventarios.js";
 
 const auth = Router(); 
 
@@ -13,6 +15,8 @@ auth.get("/generar/:categoria", requests, generateTokenSpecific, (req, res) => {
 })
 
 auth.get("/bodegas/orden", requests, customAuthPassport(Bodegas), warehouseSort);
+auth.get("/productos/inventario/cantidad", requests, customAuthPassport(Inventarios), countOfProducts)
 auth.post("/bodegas/nueva", requests, customAuthPassport(Bodegas), bodegasMD, warehousesPush); 
+
 
 export default auth; 
