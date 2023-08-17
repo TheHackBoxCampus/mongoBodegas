c7e0e41fba3e8b0f01582375072c6a8602132565
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-16 19:06:49 -0500

    feat(root): Ruta para el controlador de translado
    Se creo la ruta para ejecutar la consulta de translado de los productos

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -8,17 +8,15 @@ import { verifyPatterns } from "../controller/productos.news.js";
 import bodegasMD from "../middleware/bodegas.push.md.js";
 import Bodegas from "../storage/structure/bodegas.js";
 import Inventarios from "../storage/structure/inventarios.js";
+import { moveWarehouse } from "../controller/transladar.productos.js";
 
 const auth = Router(); 
 
-auth.get("/generar/:categoria", requests, generateTokenSpecific, (req, res) => {
-    res.status(200).send(req.categoria)
-})
-
+auth.get("/generar/:categoria", requests, generateTokenSpecific, (req, res) => res.status(200).send(req.categoria))
 auth.get("/bodegas/orden", requests, customAuthPassport(Bodegas), warehouseSort);
 auth.get("/productos/inventario/cantidad", requests, customAuthPassport(Inventarios), countOfProducts)
 auth.post("/productos/nueva", requests, customAuthPassport(Inventarios), verifyPatterns)
+auth.post("/productos/transladar", requests, customAuthPassport(Inventarios), moveWarehouse)
 auth.post("/bodegas/nueva", requests, customAuthPassport(Bodegas), bodegasMD, warehousesPush); 
-
 
 export default auth; 
